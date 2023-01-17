ce69b106fdd8e4cd0181257922de76b81692517a
timeenjoyed <timeenjoyed.twitch@gmail.com>
2023-01-17 00:03:20 -0800

    added python-dotenv

--- personal/settings.py
+++ personal/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
-from dotenv import load_dotenv, dotenv_values
+from dotenv import load_dotenv
 
 load_dotenv()
 
